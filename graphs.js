d867e3fe117be968ea7798039f416903f7ec83f9
ClaudioJPLJ <claudiojpdlj1808@gmail.com>
2022-06-20 21:26:32 -0300

    Finished project; added: sounds, animation effects, logic uptade

--- graphs.js
+++ graphs.js
@@ -1,53 +1,66 @@
-let comparation = []
-
 setTimeout(()=>{ // time to hide letters
     for (let i = 0; i != 20; i++){ 
-        document.querySelectorAll('.cards')[i].style.background = "cyan"
+        document.querySelectorAll('.cards')[i].style.background = cardColor
         document.querySelectorAll('.cards')[i].addEventListener('click', turn)
     }
 }, 2000)
 
 function turn (){ //click event 
-    if(this.style.background != 'cyan'){
+    if(this.style.background != cardColor){
         return false
     }else{
         comparation.push(this)
     } //don't click on the same card
 
-    if (comparation.length <= 2) { //click limiter 
-
-        this.style.backgroundImage = `url(imgs/img${imagesArray[this.id]}.jpg)` //added before comparison, to add image
+        if (comparation.length <= 2) { //click limiter 
+            animCard(this)
+            clickSound.play()
+            this.style.backgroundImage = `url(imgs/img${imagesArray[this.id]}.jpg)`
+             //added before comparison, to add image. Timer to run the animation first
 
             if(comparation.length == 2 && comparation[0].style.backgroundImage == comparation[1].style.backgroundImage){   //image comparator, make sure has a evens
+                even.play()
                 points++
                 comparation = []
             }
             else if (comparation.length == 2 && comparation[0].style.backgroundImage != comparation[1].style.backgroundImage){
                 setTimeout(()=>{
+                    errorSound.play()
+                },800)
+                setTimeout(()=>{
                     erros++
                     pushSpan.innerHTML = `Erros: ${erros}`
-                    comparation[0].style.background = "cyan"
-                    comparation[1].style.background = "cyan"
+                    comparation[0].style.background = cardColor
+                    comparation[1].style.background = cardColor
                     comparation = []
-                },850)
+            },1500)
             }
-    }
-    if (points == 10){
-        setTimeout(()=>{
-            alert("Você venceu! Clique em 'ok' para reiniciar")
-            comparation = []
-            combinations = []
-            divCount = 0
-            imagesArray = ["null"]
-            points = 0
-            erros = 0
-            pushSpan.innerHTML = `Erros: ${erros}`
-            randomize()
-                setTimeout(()=>{ // time to hide letters
-                for (let i = 0; i != 20; i++){ 
-                    document.querySelectorAll('.cards')[i].style.background = "cyan"
-                }
-                }, 2000)
-        }, 100)
-    }
+            if (points == 10){
+                setTimeout(()=>{
+                    alert("Você venceu! Clique em 'ok' para reiniciar")
+                    comparation = []
+                    combinations = []
+                    divCount = 0
+                    imagesArray = ["null"]
+                    points = 0
+                    erros = 0
+                    pushSpan.innerHTML = `Erros: ${erros}`
+                    randomize()
+                        setTimeout(()=>{ // time to hide letters
+                        for (let i = 0; i != 20; i++){ 
+                            document.querySelectorAll('.cards')[i].style.background = cardColor
+                        }
+                        }, 2000)
+                }, 100)
+            }
+        }
+}
+
+function animCard(target){
+        target.animate([
+        {transform: 'rotateY(180deg)'},
+        {transform: 'rotateY(0deg)'},],
+        {duration: 1000},
+        
+    )
 }
--- script.js
+++ script.js
@@ -5,11 +5,18 @@ let points = 0
 let pushSpan = document.getElementsByTagName('span')[0]
 let combinations = []
 let imagesArray = ["null"] //null for the image not to take array 0
+let comparation = []
+let cardColor = 'lemonchiffon'
+const even = new Audio("songs/even.wav")
+const clickSound = new Audio("songs/click.wav")
+const errorSound = new Audio("songs/error.mp3")
+
 
 
 for (let i = 1; i < 21; i++){
     document.body.appendChild(document.createElement('div')).setAttribute('id', i)
     document.getElementById(i).setAttribute('class', 'cards')
+    document.getElementById(i).appendChild(document.createElement('hr'))
 }
 
 randomize()
